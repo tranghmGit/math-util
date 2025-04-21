e144cbe42725344213fcbd9fbf6dc0d46a5faf8a
tranghmgit <hmtrang999@gmail.com>
2025-04-21 14:11:31 +0700

    up the main code after refactor error

--- src/com/tranghm/mathutil/core/MathUtil.java
+++ src/com/tranghm/mathutil/core/MathUtil.java
@@ -15,9 +15,9 @@ public class MathUtil {
         public static long getFactorial(int n) {
 	
             if (n < 0 || n > 20)
-                throw new IllegalArgumentException("n must be between 0 - 20!");
+                throw new IllegalArgumentException("Invalid argument. N must be between 1 to 20");
             if (n == 0 || n == 1)
 		return 1;
-            return n * getFactorial(n-1);;
+            return n * getFactorial(n-1);
 	}
 }
